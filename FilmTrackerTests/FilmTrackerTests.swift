e33fd04bc15a3ee9f2052cd21e38078782be9c23
Yunhan Yang <rivertea@gmail.com>
2015-08-30 16:42:07 +1200

    Initial Commit

---
+++ FilmTrackerTests/FilmTrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  FilmTrackerTests.swift
+//  FilmTrackerTests
+//
+//  Created by Yunhan Yang on 30/08/15.
+//  Copyright (c) 2015 Yunhan Yang. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class FilmTrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
